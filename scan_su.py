8f17fdafb416fa94db6129d6076403fd91d141fb
Yan Zhang <663yan@gmail.com>
2019-04-26 12:15:40 -0400

    device scanner

---
+++ scan_su.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+from time import sleep
+from typing import cast
+
+from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
+
+
+def on_service_state_change(
+    zeroconf: Zeroconf, service_type: str, name: str, state_change: ServiceStateChange,
+) -> None:
+    if state_change is ServiceStateChange.Added:
+        info = zeroconf.get_service_info(service_type, name)
+        if info:
+            if info.properties:
+                print(info.properties['ipv6'])
+
+if __name__ == '__main__':
+    zeroconf = Zeroconf()
+    browser = ServiceBrowser(zeroconf, "_su._tcp.local.", handlers=[on_service_state_change])
+
+    sleep(2)
+    zeroconf.close()
